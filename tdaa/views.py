273304ec5c1094ea05fb91c306763a1d691e5641
rafael lima <rafaelrlima90@hotmail.com>
2023-11-08 00:29:21 -0300

    coloquei tudo na pasta tdaa

--- tdaa/views.py
+++ tdaa/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 def home (request):
     return render (request, 'tdaa/home.html')
 def sobre (request):
-    return render (request, 'sobre.html')
+    return render (request, 'tdaa/sobre.html')
 def contato (request):
-    return render (request, 'contato.html')
+    return render (request, 'tdaa/contato.html')
 def blog (request):
-    return render (request, 'blog.html')
+    return render (request, 'tdaa/blog.html')
 
